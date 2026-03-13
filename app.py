6bc74b068e28278fe1bec9d85563260ed7e160be
kimkankamon23108-sketch <kimkankamon23108@gmail.com>
2026-03-13 21:43:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ import streamlit as st
 st.title("แผนที่วัดในจังหวัดเชียงใหม่")
 
 st.write("เว็บแสดงแผนที่วัด")
+from streamlit_folium import st_folium
+import folium
+
+m = folium.Map(location=[18.7883,98.9853], zoom_start=12)
+
+st_folium(m)
